703ac3206957099b14eb631496a66ce7ac43e068
Aleksei <aleksei.usoff@gmail.com>
2022-09-07 14:36:40 +0300

    feature/JS-5

--- src/js/weather.js
+++ src/js/weather.js
@@ -3,28 +3,25 @@ const weatherButton = document.querySelector('.weather__button');
 const myApiKey = 'http://api.weatherstack.com/current?access_key=6472d5f8eee9652e214cbb90b6688789';
 let location;
 
-const getCurrentLocation = () => {
-
-  navigator.geolocation.getCurrentPosition(pos => {
-    const { latitude, longitude } = pos.coords;
-    location = {
-      latitude,
-      longitude 
-    };
-  })
-  return location;
-}
 
 const requestApiByCity = async (city) => {
   const request = await fetch(`${myApiKey}&query=${city}`);
   const data = request.json();
+  console.log(data)
 }
 
-const requestApiByLocation = async (city) => {
-  console.log(getCurrentLocation())
-
-  //const request = await fetch(`${myApiKey}&query=${lat}&${long}`);
-  //const data = request.json();
+const getCurrentLocation = () => {
+const data = [];
+  navigator.geolocation.getCurrentPosition(function(location) {
+    data.push(location.coords.latitude);
+    data.push(location.coords.longitude);
+  });
+  return data;
+}
+const requestApiByLocation = async () => {
+  const [lat, long] = getCurrentLocation();
+  const api = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude={part}&appid={33e752ab8ec9366342da3e11c1b7625c}`;
+  console.log(api)
 }
 
 
@@ -35,4 +32,4 @@ const showWeather = (e) => {
 }
 
 weatherButton.addEventListener("click", requestApiByLocation)
-weatherInput.addEventListener("keyup", showWeather);
+weatherInput.addEventListener("keyup", showWeather)
