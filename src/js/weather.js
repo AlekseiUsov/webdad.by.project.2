96d870a64d9bf7702e0ef1cbfd9d65e6b355f273
Aleksei <aleksei.usoff@gmail.com>
2022-09-11 21:46:43 +0300

    feature/JS-5

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,60 +1,81 @@
 const weatherInput = document.querySelector('.weather__input');
-const weatherButton = document.querySelector('.weather__button');
-
-const weatherBlock = document.querySelector('.weather__block');
-const weatherInner = document.querySelector('.weather__inner');
-
-const weatherCity = document.querySelector('.weather__city');
-const weatherTemp = document.querySelector('.weather__num');
+const locationButton = document.querySelector('.weather__button');
+const arrow = document.querySelector('.weather__arrow');
 
 const weatherText = document.querySelector('.weather__text');
-const myApiKey = 'http://api.weatherstack.com/current?access_key=6472d5f8eee9652e214cbb90b6688789';
-let location;
+const weatherIcon = document.querySelector('.weather__image');
 
-const requestApiByCity = (city) => {
-  try {
-    fetch (`${myApiKey}&query=${city}`)
-    .then((response) => response.json())
-    .then((data) => {
-      weatherBlock.classList.toggle('hidden');
-      weatherCity.textContent = data.location.name;
-      weatherTemp.textContent = data.current.temperature;
-      weatherInner.classList.toggle('hidden');
-    })
-  }
-  catch(e) {
-    weatherText.classList.add(weather__text-invalid);
+const firstBlock = document.querySelector('.weather__inner-first');
+const secondBlock = document.querySelector('.weather__inner-second');
+
+const myApiKey = '33e752ab8ec9366342da3e11c1b7625c';
+let api;
+
+
+function toggleBlocks() {
+  firstBlock.classList.remove('hidden');
+  secondBlock.classList.add('hidden');
+}
+
+const weatherDetails = (data) => {
+  if (data.cod === "404") {
+    weatherText.classList.remove('hidden');
+    weatherText.innerHTML = `${weatherInput.value} isn't valid name`;
+  } else {
+    firstBlock.classList.toggle('hidden');
+    secondBlock.classList.toggle('hidden');
+    const loc = data.name;
+    const { description, id } = data.weather[0];
+    const temp = data.main.temp;
+
+    document.querySelector('.weather__city').textContent = loc;
+    document.querySelector('.weather__num').textContent = Math.floor(temp);
+    document.querySelector('.weather__descripton').textContent = description;
+
+    if (id == 800) {
+      weatherIcon.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163713.png';
+    } else if (id >= 200 && id <= 232) {
+      weatherIcon.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163683.png';
+    } else if (id >= 300 && id <= 321 || id >= 500 && id <= 531) {
+      weatherIcon.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163677.png';
+    } else if (id >= 600 && id <= 622) {
+      weatherIcon.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163705.png';
+    } else if (id >= 801 && id <= 804) {
+      weatherIcon.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163685.png';
+    } else if (id >= 701 && id <= 781) {
+      weatherIcon.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163724.png';
+    }
   }
 }
 
-const requestApiByLocation = () => {
-  let lat;
-  let long;
-
-  navigator.geolocation.getCurrentPosition(function(location) {
-    lat = location.coords.latitude;
-    long = location.coords.longitude;
-    console.log(location.coords.latitude);
-    console.log(location.coords.longitude);
-  });
-
-  const api = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&appid=33e752ab8ec9366342da3e11c1b7625c`;
-  try {
-    fetch (api)
-    .then((response) => response.json())
-    .then((data) => {console.log(data)})
-  }
-  catch(e) {
-    console.log(e)
-  }
+const requestApi = (city) => {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${myApiKey}&lang={ru}`;
+  fetch(api).then(response => response.json()).then(result => weatherDetails(result));
 }
-
 
 const showWeather = (e) => {
   if (e.key === 'Enter' && weatherInput.value !== '') {
-    requestApiByCity(weatherInput.value)
+    requestApi(weatherInput.value)
   }
 }
 
-weatherButton.addEventListener("click", requestApiByLocation)
-weatherInput.addEventListener("keyup", showWeather)
+const success = (position) => {
+  const { latitude, longitude } = position.coords;
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${myApiKey}&lang={ru}`;
+  fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+const error = (error) => {
+  console.log('error')
+}
+
+locationButton.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    console.log('Your brouser not support geolacation');
+  }
+});
+
+arrow.addEventListener('click', toggleBlocks);
+weatherInput.addEventListener("keyup", showWeather);
